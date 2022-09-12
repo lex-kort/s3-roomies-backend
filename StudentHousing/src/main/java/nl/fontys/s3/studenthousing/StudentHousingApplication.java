5fd09dcdf3977e3d3c95b381e89521d75ff92cad
Lex de Kort <488027@student.fontys.nl>
2022-09-12 15:52:12 +0200

    Change project to springboot

---
+++ StudentHousing/src/main/java/nl/fontys/s3/studenthousing/StudentHousingApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.studenthousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentHousingApplication.class, args);
+    }
+
+}
---
+++ StudentHousing/src/test/java/nl/fontys/s3/studenthousing/StudentHousingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.studenthousing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHousingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
