f5dad8c9e359abd12ba4ed8a311699b0245ebcff
Lex de Kort <488027@student.fontys.nl>
2022-09-19 09:22:33 +0200

    Showcase pipelines

--- StudentHousing/src/main/java/nl/fontys/s3/studenthousing/controller/ListingController.java
+++ StudentHousing/src/main/java/nl/fontys/s3/studenthousing/controller/ListingController.java
@@ -1,21 +1,16 @@
 package nl.fontys.s3.studenthousing.controller;
 
 import lombok.AllArgsConstructor;
-import nl.fontys.s3.studenthousing.business.converter.ListingConverter;
-import nl.fontys.s3.studenthousing.business.manager.ListingManager;
 import nl.fontys.s3.studenthousing.business.manager.impl.ListingManagerImpl;
 import nl.fontys.s3.studenthousing.domain.Listing;
 import nl.fontys.s3.studenthousing.domain.request.GetActiveListingsRequest;
 import nl.fontys.s3.studenthousing.domain.response.GetActiveListingsResponse;
-import nl.fontys.s3.studenthousing.persistence.ListingRepository;
-import nl.fontys.s3.studenthousing.persistence.entity.ListingEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -39,6 +34,7 @@ public class ListingController {
         if(listing.isEmpty()){
             return ResponseEntity.notFound().build();
         }
+        // this is a comment
         return ResponseEntity.ok().body(listing.get());
     }
 
