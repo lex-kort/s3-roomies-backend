99f69a3c9fabc52a8fbe87480901375fb462f3a1
Lex de Kort <lexdekort@live.nl>
2022-09-27 19:15:09 +0200

    - Add React Router module - Cleanup CSS

---
+++ sh-frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Listings</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
