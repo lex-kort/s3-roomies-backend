5256cc444b70d37cc9b4bf1add65114e053b14d7
Lex de Kort <lexdekort@live.nl>
2022-10-01 00:44:08 +0200

    Fix gradle build

--- sh-backend/src/main/java/nl/fontys/s3/studenthousing/controller/ListingController.java
+++ sh-backend/src/main/java/nl/fontys/s3/studenthousing/controller/ListingController.java
@@ -6,7 +6,11 @@ import nl.fontys.s3.studenthousing.domain.Listing;
 import nl.fontys.s3.studenthousing.domain.request.GetActiveListingsRequest;
 import nl.fontys.s3.studenthousing.domain.response.GetActiveListingsResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
