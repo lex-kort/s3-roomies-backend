da145be9269efaf3011d1855dd159881409634b6
Lex de Kort <488027@student.fontys.nl>
2022-09-30 22:11:20 +0200

    Update C4 diagrams

---
+++ sh-backend/src/test/java/nl/fontys/s3/studenthousing/business/manager/impl/ListingManagerImplTest.java
@@ -0,0 +1,16 @@
+package nl.fontys.s3.studenthousing.business.manager.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListingManagerImplTest {
+
+    @Test
+    void getActiveListings() {
+    }
+
+    @Test
+    void getListing() {
+    }
+}
