fa6b7c6b017f2e5e4515974c8e88638dca4712da
Lex de Kort <488027@student.fontys.nl>
2022-11-18 16:58:32 +0100

    test remove custom exceptions

--- sh-backend/src/main/java/nl/fontys/s3/studenthousing/core/exceptions/CustomException.java
+++
@@ -1,8 +0,0 @@
-package nl.fontys.s3.studenthousing.core.exceptions;
-
-public class CustomException extends Exception {
-    private final String message;
-    public CustomException(String message){
-        this.message = message;
-    }
-}
--- sh-backend/src/main/java/nl/fontys/s3/studenthousing/core/exceptions/UnauthorizedDataAccessException.java
+++
@@ -1,7 +0,0 @@
-package nl.fontys.s3.studenthousing.core.exceptions;
-
-public class UnauthorizedDataAccessException extends CustomException{
-    public UnauthorizedDataAccessException(String message){
-        super(message);
-    }
-}
